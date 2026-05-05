1c1ed174a94227c6af48dc955755803a0a5574a5
shaikchandol <shaikchandol@users.noreply.github.com>
2026-05-05 14:26:57 +0530

    Add building-blocks/RetailPos.BuildingBlocks.Domain/ValueObject.cs

---
+++ building-blocks/RetailPos.BuildingBlocks.Domain/ValueObject.cs
@@ -0,0 +1,20 @@
+namespace RetailPos.BuildingBlocks.Domain;
+
+public abstract class ValueObject
+{
+    protected abstract IEnumerable<object?> GetEqualityComponents();
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null || obj.GetType() != GetType()) return false;
+        return ((ValueObject)obj).GetEqualityComponents().SequenceEqual(GetEqualityComponents());
+    }
+
+    public override int GetHashCode() =>
+        GetEqualityComponents().Aggregate(0, (h, c) => HashCode.Combine(h, c));
+
+    public static bool operator ==(ValueObject? left, ValueObject? right) =>
+        left?.Equals(right) ?? right is null;
+
+    public static bool operator !=(ValueObject? left, ValueObject? right) => !(left == right);
+}
